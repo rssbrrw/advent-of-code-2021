4de2347c9f4718187e086e788c9b1d22eec13d80
Ross Barrow <ross.barrow@boughtbymany.com>
2021-12-04 14:07:21 +0000

    AoC 2021 day 3 part 2 in Haskell

--- 03/3.hs
+++ 03/3.hs
@@ -12,9 +12,32 @@ leastCommon :: (Eq a, Ord a) => [a] -> a
 leastCommon = head . minimumBy (comparing length) . group . sort
 
 taskOne :: [String] -> Int
-taskOne s = binToDec (map mostCommon $ transpose s) * binToDec (map leastCommon $ transpose s)
+taskOne ss = binToDec (map mostCommon $ transpose ss) * binToDec (map leastCommon $ transpose ss)
+
+oxygen :: [String] -> Int
+oxygen ss = binToDec $ head $ foldl filterMostCommonAtIndex ss [0..length (head ss) - 1]
+
+co2 :: [String] -> Int
+co2 ss = binToDec $ head $ foldl filterLeastCommonAtIndex ss [0..length (head ss) - 1]
+
+taskTwo :: [String] -> Int
+taskTwo ss = oxygen ss * co2 ss
+
+-- | For a matrix and index i, return only the rows from the matrix
+--   whose element at i is the most common at that index
+filterMostCommonAtIndex :: [String] -> Int -> [String]
+filterMostCommonAtIndex ss i = filterForIndex ss i mostCommon
+
+-- | For a matrix and index i, return only the rows from the matrix
+--   whose element at i is the least common at that index
+filterLeastCommonAtIndex :: [String] -> Int -> [String]
+filterLeastCommonAtIndex ss i = filterForIndex ss i leastCommon
+
+filterForIndex :: [String] -> Int -> (String -> Char) -> [String]
+filterForIndex ss i func = filter (\w -> (w !! i) == func (transpose ss !! i)) ss
 
 main = do
   input <- getContents
   let numbers = lines input
   print $ taskOne numbers
+  print $ taskTwo numbers
